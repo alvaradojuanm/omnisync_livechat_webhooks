156eac892c2ca4ca8a95cf7da79e0a65dbba91a1
Juan Alvarado <alvaradojuanm@gmail.com>
2025-07-04 23:05:37 -0400

    __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'OmniSync LiveChat Webhooks',
+    'version': '18.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Integración de Webhooks para LiveChat',
+    'description': 'Este módulo permite la integración de webhooks para LiveChat en Odoo.',
+    'author': 'Tu Nombre o Empresa',
+    'website': 'https://github.com/alvaradojuanm/omnisync_livechat_webhooks',
+    'license': 'AGPL-3',
+    'depends': ['base', 'im_livechat'],
+    'data': [
+        # Aquí van los archivos XML, CSV, etc., que el módulo necesita cargar
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
